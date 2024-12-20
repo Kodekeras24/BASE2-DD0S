e61ccd42499d95570fb0763161eae24dbe3472b8
Kodekeras24 <zhanahmad57@gmail.com>
2024-12-20 17:13:42 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ c = input("Choose your environment: [0] pip / [1] pip3 : ")
 
 if c == "0":
     os.system("pip install click")
-    os.system("pip install")
-    
+    os.system("pip install fake_headers")
 elif c == "1":
-
+    os.system("pip3 install click")
+    os.system("pip3 install fake_headers")
 print("Done.")
